1b715b6067ef5797189aff30d3b99f079259641c
abrahanlopez878-sudo <abrahanlopez878@gmail.com>
2026-04-17 10:50:52 -0400

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<?php
+echo '<nav class="navbar">
+    <div class="nav-container">
+        <a href="index.php" class="logo-container">
+            <img src="IUJO.gif" alt="Logo LIVElula" class="logo-img">
+          
+        </a>
+
+        <ul class="nav-menu">
+            <li><a href="index.php" class="nav-link">Inicio</a></li>
+            <li><a href="equipo.html" class="nav-link">Acerca de</a></li>
+            <li><a href="warp.php" class="nav-link"">Configuración</a></li>
+        </ul>
+        
+
+    </div>
+</nav>';
+?>
