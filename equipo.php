73f100ce8312b62ba9d3552b99ea21a43faffb73
CPAaronG <aaron_yorck@hotmail.com>
2026-04-17 22:11:51 -0400

    17/4/2026 10-11pm

--- equipo.php
+++ equipo.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LIVElula | Nuestro Equipo</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="nosotros.css">
+    <link rel="stylesheet" href="equipo.css">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
